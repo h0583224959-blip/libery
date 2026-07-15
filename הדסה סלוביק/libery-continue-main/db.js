cffe90960883927daa95afde28f44da8749cc4a6
h0583224959-blip <h0583224959@gmail.com>
2026-07-16 00:21:14 +0300

    Delete הדסה סלוביק/libery-continue-main directory

--- הדסה סלוביק/libery-continue-main/db.js
+++
@@ -1,7 +0,0 @@
-const books = [
-    { id: 1, name: "The Hobbit", category: "Fantasy", price: 15.99, isBorrowed: false, borrowHistory: [] },
-    { id: 2, name: "1984", category: "Science Fiction", price: 13.99, isBorrowed: false, borrowHistory: [] },
-    { id: 3, name: "To Kill a Mockingbird", category: "Classic", price: 18.99, isBorrowed: false, borrowHistory: [] }
-];
-
-module.exports = { books };
